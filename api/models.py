d7dc40f0d72215965519e5c56bc065a2311c4f58
Abasp <abaspoor@hotmail.com>
2023-07-23 14:43:31 +0330

    install package Pillow

--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, UserManager
 
 def upload_path_handler(instance, filename):
     return "avatars/{id}/{file}".format(id=instance.user.id, file=filename)
