a8a541c6872d894775d93a14a90844d2c985feea
Abasp <abaspoor@hotmail.com>
2023-07-24 21:52:19 +0330

    ok

--- bwf/settings.py
+++ bwf/settings.py
@@ -75,7 +75,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bwf.wsgi.application'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:10961"
+    "http://localhost:5501"
     ]
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
